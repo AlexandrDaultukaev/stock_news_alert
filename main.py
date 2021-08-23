e329a9600b05cb4184a851a2b42c4aa11af2f3cb
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-23 23:14:16 +0700

    Add comparsion yesterday's stock price and today's stock price

--- main.py
+++ main.py
@@ -4,14 +4,42 @@ from dotenv import load_dotenv
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla"
+news_URL = "https://newsapi.org/v2/top-headlines"
+stock_URL = "https://www.alphavantage.co/query"
 
 load_dotenv()
-API = os.getenv("API_KEY")
+news_API = os.getenv("API_KEY")
+stock_API = os.getenv("STOCK_API")
 
-params = {
-    "apiKey": API,
+news_params = {
+    "apiKey": news_API,
     "q": COMPANY_NAME,
 }
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": stock_API,
+}
+
+
+def find_percentage(stk):
+    day = int(list(stk["Time Series (Daily)"].keys())[0].split("-")[2])
+    month = list(stk["Time Series (Daily)"].keys())[0].split("-")[1]
+    year = list(stk["Time Series (Daily)"].keys())[0].split("-")[0]
+    print(stk)
+    yesterday = day - 1
+    if day < 10:
+        day = f"0{day}"
+    if yesterday < 10:
+        yesterday = f"0{yesterday}"
+
+    if float(stk["Time Series (Daily)"][f"{year}-{month}-{day}"]["4. close"]) > \
+            float(stk["Time Series (Daily)"][f"{year}-{month}-{yesterday}"]["4. close"]):
+        print("Stock price increase")
+    else:
+        print("Stock price decrease")
+
 
 
 def find_description(art):
@@ -20,10 +48,15 @@ def find_description(art):
             return num_article
 
 
-response = requests.get("https://newsapi.org/v2/top-headlines", params=params)
+response = requests.get(news_URL, params=news_params)
 response.raise_for_status()
-
 articles = response.json()
+
+response = requests.get(stock_URL, params=stock_params)
+response.raise_for_status()
+stock_news = response.json()
+find_percentage(stock_news)
+
 num_art = find_description(articles)
 title = articles["articles"][num_art]["title"]
 description = articles["articles"][num_art]["description"]
